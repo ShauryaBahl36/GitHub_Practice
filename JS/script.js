beac64a94a1e4322f072dd0aa958ebfd5e2f6f67
Bahl_Shaurya <bahl.shaurya02@gmail.com>
2025-06-22 16:31:00 +0530

    Introduction of JS

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+// console.log('Shaurya')
+// console.warn('warn')
+// console.error('error')
+// console.info('info')
+
+// Keywords
+let a=20
+const b=30
+var c=40
+
+// // b=40
+// c=50
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Single line comment
+/* Multi-Line
+Comment */
+
+let x=15;
+let y=30;
+let z=60;
+
+console.log(x+y-z);
+
+console.log(y=='30');
+console.log(y==='30');
+
+console.log(10<<2);
+
+let fname='Lucas';
+let lname='Makovich';
+let fullname=fname+lname;
+console.log(fullname);
+
+let j = true ? 10 : 20;
+console.log(j);
+console.log(typeof fname);
