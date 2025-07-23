bf8e6f2d095ff97b17b8a93df079bf9f602bc3a1
Bahl_Shaurya <bahl.shaurya02@gmail.com>
2025-07-23 23:37:46 +0530

    JS Practice

--- JS/script.js
+++ JS/script.js
@@ -273,6 +273,30 @@ function goRight() {
 
 */
 
+/* Code for Practice:
+function getMilk(bottles) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " + bottles + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+*/
+
 let i='a';
 switch(i){
     case 'a':
